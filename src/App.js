e7fe7540eb5ca5a7f36ba032ba313b436bfe9787
Gstv <gstv.almeida09@gmail.com>
2020-10-26 17:10:05 -0300

    referencias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState, createRef } from 'react';
 
 import capturar01 from './assets/videos/capturar/1.0_EAN_completo.mp4';
 import capturar02 from './assets/videos/capturar/2.0_ITF_completo.mp4';
@@ -15,7 +15,7 @@ import Video from './components/Video'
 
 /*
 [x] Keydown
-[ ] Referenciar cada video
+[x] Referenciar cada video
 [x] Trocar os videos ao apertar outro botao
 [ ] Deixar os videos passando sozinhos
 [ ] Screensaver
@@ -33,25 +33,67 @@ function App() {
   });
 
   const [category, setCategory] = useState([]);
+  const [elRefs, setElRefs] = useState([]);
+  const [isEnter, setIsEnter] = useState({capturar: false, compartilhar: false, identificar: false})
   const capturarVideos = [capturar01, capturar02, capturar03, capturar04];
   const compartilharVideos = [compartilhar01, compartilhar02, compartilhar03, compartilhar04];
+  var index = 0;
 
   function handleKeyDown(e) {
+
     if (e.keyCode === 49) {
+      const length = capturarVideos.length;
+      // const arr = Array(length).fill(createRef());
+      setElRefs(elRefs => (
+        Array(length).fill().map((_, i) => elRefs[i] || createRef())
+      ));
+
+      // setElRefs([...arr]);
       setCategory([...capturarVideos]);
-      console.log(category)
+      setIsEnter({
+        capturar: true,
+        compartilhar: false,
+        identificar: false,
+      });
+      // handleNext(elRefs);
     }
+
     else if (e.keyCode === 50) {
+      const length = compartilharVideos.length;
+      const arr = Array(length).fill(createRef());
+
+      setElRefs([...arr]);
       setCategory([...compartilharVideos]);
-      console.log(category)
+      setIsEnter({
+        capturar: false,
+        compartilhar: true,
+        identificar: false,
+      });
     }
+
     else if (e.keyCode === 51) {
     }
   };
 
+  // function handleNext(elRefs) {
+  //   console.log(elRefs[index]?.current);
+  //   elRefs[0].current.classList.add('active')
+  //   // elRefs.map((atual) => {
+  //   //   console.log(atual.current)
+  //   //   return atual.current;
+  //   // })
+  //   index++;
+  // }
+
+  useEffect((index) => {
+    console.log(elRefs[index]?.current)
+    // elRefs[index].current.classList.add('active')
+    index++;
+  }, [elRefs]);
+
   return (
     <>
-      <Video category={category}/>
+      <Video category={category} elRefs={elRefs}/>
     </>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-function Video({ category }) {
+function Video({ category, elRefs }) {
   return (
     <div>
+      {console.log('alou ref: ', elRefs)}
+      {/* {elRefs[0]?.current?.classList.add('active')} */}
       {category.map((current, index) => (
-        <video key={index} src={current} type="video/mp4" autoPlay muted />
+        <video key={index} ref={elRefs[index]} src={current} type="video/mp4" autoPlay muted />
       ))}
     </div>
   );
